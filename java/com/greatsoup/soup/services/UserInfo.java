6a9dab189650f244c72ae5643e267ec3f98aa118
Wiaci <61415128+Wiaci@users.noreply.github.com>
2021-02-17 17:23:13 +0300

    Update UserInfo.java

--- java/com/greatsoup/soup/services/UserInfo.java
+++ java/com/greatsoup/soup/services/UserInfo.java
@@ -10,11 +10,9 @@ import java.util.Random;
 public class UserInfo {
 
     private String login;
-    private final int id;
 
     public UserInfo() {
         login = "";
-        id = new Random().nextInt(100);
     }
 
     public String getLogin() {
